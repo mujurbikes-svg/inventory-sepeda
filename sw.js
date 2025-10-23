59a3a22f318cf026cd97c3470758aa3b64b2e96f
mujurbikes-svg <mujurbikes@gmail.com>
2025-10-23 08:26:25 +0800

    Add service worker for offline caching
    Implement service worker for caching assets and offline functionality.

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
+// Service Worker untuk offline functionality
+const CACHE_NAME = 'inventory-sepeda-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/app.js',
+    '/js/database.js',
+    '/js/print.js',
+    '/js/qrcode.js',
+    '/manifest.json'
+];
 
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Return cached version or fetch from network
+                return response || fetch(event.request);
+            }
+        )
+    );
+});
